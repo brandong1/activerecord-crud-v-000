afb523955d7e03927d5041b6ca8d93996d5a1b8b
Brandon Green <brandon@brandonleeproductions.com>
2018-08-22 01:51:16 +0000

    automatically backed up by learn

--- db/migrate/20180822014915_create_movies.rb
+++ db/migrate/20180822014915_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
+    add_column :create_movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
   end
 end
